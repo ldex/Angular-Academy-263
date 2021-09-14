343538250f20059b27b3f525a14d71aecb850626
ldex <laurent.duveau+git@gmail.com>
2021-09-14 14:52:09 -0400

    Fin du jour 2

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
 
 @Component({
@@ -10,9 +12,37 @@ export class ProductDetailComponent implements OnInit {
 
   @Input() product: Product;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private router: Router
+  ) { }
+
+  delete() {
+    if(window.confirm('Are you sure ??')) {
+      this
+      .productService
+      .deleteProduct(this.product.id)
+      .subscribe(
+        () => {
+          console.log('Product deleted from server.');
+          this.productService.initProducts();
+          this.router.navigateByUrl('/products');
+        },
+        error => console.log('Could not delete product with error:' + error)
+      )
+    }
+  }
 
   ngOnInit(): void {
+    let id = this.activatedRoute.snapshot.params['id'];
+
+    this
+      .productService
+      .getProductById(id)
+      .subscribe(
+        result => this.product = result
+      );
   }
 
 }
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { EMPTY, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product.service';
@@ -17,11 +18,34 @@ export class ProductListComponent implements OnInit {
   selectedProduct: Product;
   errorMessage: string;
 
-  onSelect(product: Product) {
-    this.selectedProduct = product;
+  // Pagination
+  pageSize = 5;
+  start = 0;
+  end = this.pageSize;
+  currentPage = 1;
+
+  previousPage() {
+    this.start -= this.pageSize;
+    this.end -= this.pageSize;
+    this.currentPage--;
+    this.selectedProduct = null;
   }
 
-  constructor(private productService: ProductService) {
+  nextPage() {
+    this.start += this.pageSize;
+    this.end += this.pageSize;
+    this.currentPage++;
+    this.selectedProduct = null;
+  }
+
+  onSelect(product: Product) {
+    this.selectedProduct = product;
+    this.router.navigateByUrl('/products/' + product.id);
+  }
+
+  constructor(
+    private productService: ProductService,
+    private router: Router) {
 
   }
 
